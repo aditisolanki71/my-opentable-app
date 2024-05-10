450eb1ca8a22693205470161a0e789a7b6d2c41d
Aditi <adtsolanki22@gmail.com>
2024-05-10 19:00:01 +0530

    code cleanup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,9 @@
-import NavBar from "./components/NavBar";
-import AuthContext from "./context/AuthContext";
-import "./globals.css";
-import "react-datepicker/dist/react-datepicker.css";
+import './globals.css'
+
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
   return (
     <html lang="en">
@@ -14,16 +12,7 @@ export default function RootLayout({
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>
-        <main className="bg-gray-100 min-h-screen w-screen">
-          <AuthContext>
-            <main className="max-w-screen-2xl m-auto bg-white">
-              <NavBar />
-              {children}
-            </main>
-          </AuthContext>
-        </main>
-      </body>
+      <body>{children}</body>
     </html>
-  );
+  )
 }
