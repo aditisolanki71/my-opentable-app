29c9b5b15a19c1f5b9f3e51b2b7bb34570fb1830
Aditi <adtsolanki22@gmail.com>
2024-05-18 17:23:27 +0530

    Validation for User fields

--- pages/api/auth/signup.ts
+++ pages/api/auth/signup.ts
@@ -1,5 +1,6 @@
 import { PrismaClient } from "@prisma/client";
 import { NextApiRequest, NextApiResponse } from "next";
+import validator from "validator";
 
 const prisma = new PrismaClient();
 
@@ -12,8 +13,65 @@ export default async function handler(
 ) {
   //POST Request
   if (req.method === "POST") {
+    const body = req.body;
+    const { firstName, lastName, email, city, phone, password } = body;
+    const errors: string[] = [];
+
+    const validationSchema = [
+      {
+        valid: validator.isLength(firstName, {
+          min: 1,
+          max: 20,
+        }),
+        errorMessage: "First Name is Invalid",
+      },
+      {
+        valid: validator.isLength(lastName, {
+          min: 1,
+          max: 20,
+        }),
+        errorMessage: "Last Name is Invalid",
+      },
+      {
+        valid: validator.isEmail(email),
+        errorMessage: "Email is Invalid",
+      },
+      {
+        valid: validator.isMobilePhone(phone),
+        errorMessage: "Phone is Invalid",
+      },
+      {
+        valid: validator.isLength(city, { min: 1 }),
+        errorMessage: "City is Invalid",
+      },
+      {
+        valid: validator.isStrongPassword(password),
+        errorMessage: "Password is not Strong",
+      },
+    ];
+
+    validationSchema.forEach((check) => {
+      if (!check.valid) {
+        errors.push(check.errorMessage);
+      }
+    });
+
+    if (errors.length) {
+      return res.status(400).json({ errorMessage: errors[0] });
+    }
+
+    const userWithEmail = await prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    if (userWithEmail) {
+      return res.status(400).json({ errorMessage: "Email already in used" });
+    }
+
     return res.status(400).json({
-      hello: "Aditi",
+      hello: body,
     });
   }
 }
