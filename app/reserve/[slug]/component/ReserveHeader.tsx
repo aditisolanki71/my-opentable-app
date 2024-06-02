f90620601cbf0051701d18711ce8ee6d22daf4b5
Aditi <adtsolanki22@gmail.com>
2024-06-02 15:34:17 +0530

    render data for the reserve header

--- app/reserve/[slug]/component/ReserveHeader.tsx
+++ app/reserve/[slug]/component/ReserveHeader.tsx
@@ -1,17 +1,18 @@
 import Reserve from "../page";
-
-const ReserveHeader = () => {
+interface ReserveHeader {
+  image: string;
+  name: string;
+}
+const ReserveHeader = (props: ReserveHeader) => {
+  const { image, name } = props;
+  console.log("reserve header", props);
   return (
     <div>
       <h3 className="font-bold">You're almost done!</h3>
       <div className="mt-5 flex">
-        <img
-          src="https://images.otstatic.com/prod1/49153814/2/medium.jpg"
-          alt=""
-          className="w-32 h-18 rounded"
-        />
+        <img src={image} alt="" className="w-32 h-24 rounded" />
         <div className="ml-4">
-          <h1 className="text-3xl font-bold">Aiāna Restaurant Collective</h1>
+          <h1 className="text-3xl font-bold">{name}</h1>
           <div className="flex mt-3">
             <p className="mr-6">Tues, 22, 2023</p>
             <p className="mr-6">7:30 PM</p>
--- app/reserve/[slug]/page.tsx
+++ app/reserve/[slug]/page.tsx
@@ -1,11 +1,29 @@
 import ReserveHeader from "./component/ReserveHeader";
 import ReserveForm from "./component/ReserveForm";
+import { PrismaClient } from "@prisma/client";
+import { notFound } from "next/navigation";
 
-const Reserve = () => {
+const prisma = new PrismaClient();
+
+const fetchRestaurantBySlug = async (slug: string) => {
+  const restaurant = await prisma.restaurant.findUnique({
+    where: {
+      slug,
+    },
+  });
+
+  if (!restaurant) {
+    notFound();
+  }
+  return restaurant;
+};
+const Reserve = async ({ params }: { params: { slug: string } }) => {
+  const restaurant = await fetchRestaurantBySlug(params.slug);
+  console.log("reserve page", restaurant);
   return (
     <div className="border-t h-screen">
       <div className="py-9 w-3/5 m-auto">
-        <ReserveHeader />
+        <ReserveHeader image={restaurant.main_image} name={restaurant.name} />
         <ReserveForm />
       </div>
     </div>
