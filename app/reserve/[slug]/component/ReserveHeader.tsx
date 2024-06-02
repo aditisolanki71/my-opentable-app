6123f5d1c80a3e112d9a911e32df920d3ba0616f
Aditi <adtsolanki22@gmail.com>
2024-06-02 15:42:38 +0530

    extract query params

--- app/reserve/[slug]/component/ReserveHeader.tsx
+++ app/reserve/[slug]/component/ReserveHeader.tsx
@@ -1,11 +1,17 @@
+import { format } from "date-fns";
+import { convertToDisplayTime } from "../../../utils/ConvertToDisplayTime";
 import Reserve from "../page";
 interface ReserveHeader {
   image: string;
   name: string;
+  date: string;
+  partySize: string;
 }
 const ReserveHeader = (props: ReserveHeader) => {
-  const { image, name } = props;
+  const { image, name, date, partySize } = props;
   console.log("reserve header", props);
+
+  const [day, time] = date.split("T");
   return (
     <div>
       <h3 className="font-bold">You're almost done!</h3>
@@ -14,9 +20,11 @@ const ReserveHeader = (props: ReserveHeader) => {
         <div className="ml-4">
           <h1 className="text-3xl font-bold">{name}</h1>
           <div className="flex mt-3">
-            <p className="mr-6">Tues, 22, 2023</p>
-            <p className="mr-6">7:30 PM</p>
-            <p className="mr-6">3 people</p>
+            <p className="mr-6">{format(new Date(date), "ccc, LLL d")}</p>
+            <p className="mr-6">{convertToDisplayTime(time)}</p>
+            <p className="mr-6">
+              {partySize} {parseInt(partySize) > 1 ? "Person" : "pPeople"}{" "}
+            </p>
           </div>
         </div>
       </div>
--- app/reserve/[slug]/page.tsx
+++ app/reserve/[slug]/page.tsx
@@ -17,13 +17,25 @@ const fetchRestaurantBySlug = async (slug: string) => {
   }
   return restaurant;
 };
-const Reserve = async ({ params }: { params: { slug: string } }) => {
+const Reserve = async ({
+  params,
+  searchParams,
+}: {
+  params: { slug: string };
+  searchParams: { date: string; partySize: string };
+}) => {
   const restaurant = await fetchRestaurantBySlug(params.slug);
   console.log("reserve page", restaurant);
+
   return (
     <div className="border-t h-screen">
       <div className="py-9 w-3/5 m-auto">
-        <ReserveHeader image={restaurant.main_image} name={restaurant.name} />
+        <ReserveHeader
+          image={restaurant.main_image}
+          name={restaurant.name}
+          date={searchParams.date}
+          partySize={searchParams.partySize}
+        />
         <ReserveForm />
       </div>
     </div>
